9fc7220501d8bc97f934848b07bdd68106dc0228
KisonHe <kisonmaxine@gmail.com>
2020-12-16 20:59:44 +0800

    KPE V1

---
+++ esp32/MagicMirrorESP32/components/KeyPressEmulator/include/keyPressEmulator.h
@@ -0,0 +1,33 @@
+/**
+ * @brief Nothing
+ *
+ * @ingroup PackageName
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @note Must use in rtos
+ * 
+ * @author KisonHe
+ * Contact: 
+ *
+ */
+#ifndef KEYPRESSEMULATOR_H
+#define KEYPRESSEMULATOR_H
+
+// int createStartKPETimer();
+
+class keyPressEmulator
+{
+private:
+    int gpioNum;
+    int isHigh2Press;
+    int _deinitPin();
+    int _initPin();
+    bool lock;
+public:
+    int doPress();
+    keyPressEmulator(int gpioNum_, int isHigh2Press_);
+    ~keyPressEmulator();
+};
+
+
+#endif
