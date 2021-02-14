b4bc6f16872942b6aea24f4ff4c73fa421214df7
KisonHe <kisonmaxine@gmail.com>
2021-02-14 10:38:17 +0800

    Lots of things

--- raspberry_pi/systemd/mirrord.sh
+++ raspberry_pi/systemd/mirrord.sh
@@ -1,16 +1,18 @@
-# note
-# this is public repo so dont write the host and port in sh. Put them in .env and edit them
-# launch chromium
-chromium-browser -noerrdialogs -kiosk -start_maximized  --disable-infobars --app=$host:$port  --ignore-certificate-errors-spki-list --ignore-ssl-errors --ignore-certificate-errors 2>/dev/null
+#!/bin/bash
+export DISPLAY=:0 #change this to auto detect later
 
+# note
+# Put host and port in .env and edit them
+# launch chromium
+#/usr/bin/chromium-browser -noerrdialogs -kiosk -start_maximized  --disable-infobars --app=$host:$port  --ignore-certificate-errors-spki-list --ignore-ssl-errors --ignore-certificate-errors 2>/dev/null
+/usr/bin/chromium-browser -noerrdialogs -kiosk -start_maximized  --disable-infobars --app=$host:$port  2>/dev/null
 
 # force refresh page around a time
 # [TODO]
 
-# shutdown gracely if need to, in case schools cuts power.
-# [TODO]
-# put args in .env
+# shutdown gracely, in case schools cuts power.
+# shutdown -h 23:00
 
 # https://www.raspberrypi.org/documentation/raspbian/applications/vcgencmd.md
-vcgencmd display_power 0 # turn off 
-vcgencmd display_power 1 # turn on
+# vcgencmd display_power 0 # turn off 
+# vcgencmd display_power 1 # turn on
---
+++ raspberry_pi/systemd/startMirror.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /root/MagicMirror
+npm run server
